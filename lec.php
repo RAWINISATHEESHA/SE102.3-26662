75d33f42e777549465ba783fa1ffe51f91fc4a29
RAWINI SATHEESHA <134356654+RAWINISATHEESHA@users.noreply.github.com>
2023-07-12 16:12:47 +0530

    Add files via upload

---
+++ lec.php
@@ -0,0 +1,11 @@
+<html>
+    <body>
+<h3>Uploading a file</h3>
+<h4>Select the file to be uploaded</h4><br>
+<form name=“form1” action="http://localhost/uploadfile.php" method="POST" enctype="multipart/form-data">
+<input type="file" name="file1" size="50">
+<br>
+<input type="SUBMIT" name="submit" value="Send File">
+</form>
+</body>
+</html>
